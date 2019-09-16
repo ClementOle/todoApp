a8f4a9aa612608357d473b226522a17380f3245c
Clément Olewski <clement.olewski@gmail.com>
2019-09-16 15:45:24 +0200

    Séparation des méthodes lié à la base de donnés dans un package service

--- app/src/main/java/com/olewski/myapplication/Database/SQLiteDataBaseHelper.java
+++ app/src/main/java/com/olewski/myapplication/Database/SQLiteDataBaseHelper.java
@@ -8,21 +8,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import com.olewski.myapplication.Model.Task;
 
+import static com.olewski.myapplication.Services.ListService.*;
+import static com.olewski.myapplication.Services.TaskService.*;
+
 public class SQLiteDataBaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "Todo.db";
-    //Task
-    public static final String TABLE_TASK_NAME = "TASK";
-    public static final String COL_ID_TASK = "ID";
-    public static final String COL_TEXT_TASK = "TEXT";
-    public static final String COL_ISDONE_TASK = "ISDONE";
-    public static final String COL_LIST_ID_TASK = "LISTID";
-
-    //List
-
-    public static final String TABLE_LIST_NAME = "LIST";
-    public static final String COL_ID_LIST = "ID";
-    public static final String COL_TEXT_LIST = "TEXT";
 
 
     public SQLiteDataBaseHelper(Context context) {
@@ -47,48 +38,4 @@ public class SQLiteDataBaseHelper extends SQLiteOpenHelper {
 
     }
 
-    public boolean newList(String text) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_TEXT_LIST, text);
-        long result = db.insert(TABLE_LIST_NAME, null, contentValues);
-        if (result == -1)
-            return false;
-        else
-            return true;
-    }
-
-    public Cursor getAllList() {
-        SQLiteDatabase db = this.getWritableDatabase();
-        return db.rawQuery("SELECT * FROM " + TABLE_LIST_NAME, null);
-    }
-
-    public boolean newTask(Task task) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_TEXT_TASK, task.getText());
-        contentValues.put(COL_ISDONE_TASK, task.getIsDone());
-        contentValues.put(COL_LIST_ID_TASK, task.getListId());
-        long result = db.insert(TABLE_TASK_NAME, null, contentValues);
-        if (result == -1)
-            return false;
-        else
-            return true;
-    }
-
-    public Cursor getAllTasks() {
-        SQLiteDatabase db = this.getWritableDatabase();
-        return db.rawQuery("SELECT * FROM " + TABLE_TASK_NAME, null);
-    }
-
-    public Cursor getAllTasksByListId(Integer listId) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        return db.rawQuery("SELECT * FROM " + TABLE_TASK_NAME + " WHERE " + COL_LIST_ID_TASK + " = " + listId, null);
-    }
-
-    public boolean updateTask(Task task) {
-        SQLiteDatabase db = this.getWritableDatabase();
-       // db.rawQuery("UPDATE ")
-        return true;
-    }
 }
